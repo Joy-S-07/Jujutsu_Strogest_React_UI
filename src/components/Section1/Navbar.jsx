22286312922d3c9d8c5f83fbd42388d4ee2bc719
Joy Sengupta <joysengupta521@gmail.com>
2026-03-16 00:12:30 +0530

    Update Navbar.jsx

--- src/components/Section1/Navbar.jsx
+++ src/components/Section1/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <h3 className="text-white rounded-full w-1/12 flex items-center justify-center h-[50] py-2 font-bold text-4xl">呪術廻戦</h3>
         <div className='flex gap-5 text-lg'>
           <a href="https://www.linkedin.com/in/beinggojo/"><i className="ri-linkedin-fill text-white"></i></a>
-          <a href="https://github.com/joy-sengupta"><i className="ri-github-fill text-white flex"></i> </a>
+          <a href="https://github.com/Joy-S-07"><i className="ri-github-fill text-white flex"></i> </a>
         <h3 className="text-white">
           Developed By Joy Sengupta
         </h3>
