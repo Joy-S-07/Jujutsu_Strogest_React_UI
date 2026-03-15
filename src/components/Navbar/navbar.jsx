a6f14add5fec5c836c7503cce4d2fb93684fef72
Joy Sengupta <joysengupta521@gmail.com>
2026-03-15 23:34:05 +0530

    Update navbar.jsx

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='justify-between flex px-10 py-5'>
-        <h3 className="text-white bg-black rounded-full w-1/12 flex items-center justify-center w-1/2 h-[50] py-2 font-bold">Target Audience</h3>
-        <button className="text-gray-600 font-bold flex uppercase">digital marketing platform</button>
+    <div className='justify-between flex px-10 py-5 uppercase font-bold'>
+        <h3 className="text-white rounded-full w-1/12 flex items-center justify-center h-[50] py-2 font-bold text-4xl">呪術廻戦</h3>
+        <div className='flex gap-5 text-lg'>
+          <a href="https://www.linkedin.com/in/beinggojo/"><i className="ri-linkedin-fill text-white"></i></a>
+          <a href="https://github.com/Joy-S-07"><i className="ri-github-fill text-white flex"></i> </a>
+        <h3 className="text-white">
+          Developed By Joy Sengupta
+        </h3>
+        </div>
     </div>
   )
 }
